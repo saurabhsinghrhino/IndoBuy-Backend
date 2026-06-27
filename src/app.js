19042d7b211a059b6c9afb68e0b0e0a8ab636bf3
Saurabh singh <saurabhsingh941576@gmail.com>
2026-06-27 12:24:06 +0530

    change in the Origin Url

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const paymentRoutes = require("./routes/payment.routes");
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://indo-buy.vercel.app/",
+    origin: "https://indo-buy.vercel.app",
     credentials: true,
   }),
 );
